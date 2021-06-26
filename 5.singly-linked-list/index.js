d272d4f2cc2192c3c2a1c35d83693e884857209a
Felipe Chernicharo <string7dev@gmail.com>
2021-06-25 22:13:40 -0400

    reversed linked list

--- 5.singly-linked-list/index.js
+++ 5.singly-linked-list/index.js
@@ -90,82 +90,107 @@ class SinglyLinkedList {
     }
     return this.head;
   }
+
+  get(num) {
+    if (num < 0 || num >= this.length) return undefined;
+    if (num === 0) return this.head;
+
+    let curr = this.head;
+    let count = 0;
+    while (count <= num) {
+      if (count === num) return curr;
+      curr = curr.next;
+      count++;
+    }
+  }
+
+  set(val, index) {
+    const foundNode = this.get(index);
+
+    if (foundNode) {
+      foundNode.val = val;
+      return foundNode;
+    }
+
+    return undefined;
+  }
+
+  print() {
+    const arr = [];
+    let current = this.head;
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === 0 || i === this.length - 1) arr.push(`|${current.val}|`);
+      else {
+        arr.push(current.val);
+      }
+
+      current = current.next;
+    }
+
+    console.log(arr);
+  }
+
+  insert(val, index) {
+    if (index < 0 || index >= this.length)
+      return new Error("index doesn't exist: Couldn't insert value to list");
+
+    if (index === 0) return this.unshift(val);
+    if (index === this.length - 1) return this.push(val);
+
+    const newNode = new Node(val);
+    const nextNode = this.get(index);
+    const prevNode = this.get(index - 1);
+    prevNode.next = newNode;
+    newNode.next = nextNode;
+
+    // console.log({ prevNode, nextNode, newNode });
+
+    this.length++;
+    return newNode;
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length)
+      return new Error("index doesn't exist: Couldn't remove value to list");
+
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    const removedNode = this.get(index);
+    const prevNode = this.get(index - 1);
+    const nextNode = prevNode.next;
+
+    prevNode.next = nextNode;
+
+    this.length--;
+
+    return removedNode;
+  }
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+    // node => 0
+
+    for (let i = 0; i < this.length; i++) {
+      next = node.next; // next =>    1 => 2  => 3
+      node.next = prev; // prev => null => 1 => 2
+      prev = node; // prev =>         1 => 2 => 3
+      node = next; // node =>         0 => 1 => 2
+    }
+
+    return this;
+  }
 }
 
 const list = new SinglyLinkedList();
 
-// list.push("hi");
-// list.push("hola");
-// list.push(45);
-// list.push(80);
-// console.log("------------------------------------------");
-// console.log({ list });
-// console.log("---------------- POP! -------------------");
-// console.log(list.pop());
-// console.log("------------------------------------------");
-// console.log({ list });
-// console.log("---------------- POP! -------------------");
-// console.log(list.pop());
-// console.log("------------------------------------------");
-// console.log({ list });
-// console.log("---------------- POP! -------------------");
-// console.log(list.pop());
-// console.log("------------------------------------------");
-// console.log({ list });
-// console.log("---------------- POP! -------------------");
-// console.log(list.pop());
-// console.log("------------------------------------------");
-// console.log({ list });
-// console.log("---------------- SHIFT! -------------------");
-// console.log(list.shift());
-// console.log("------------------------------------------");
-// console.log({ list });
-// console.log("---------------- SHIFT! -------------------");
-// console.log(list.shift());
-// console.log("------------------------------------------");
-// console.log({ list });
-// console.log("---------------- SHIFT! -------------------");
-// console.log(list.shift());
-// console.log("------------------------------------------");
-// console.log({ list });
-// console.log("---------------- SHIFT! -------------------");
-// console.log(list.shift());
-// console.log("------------------------------------------");
-// console.log({ list });
-// console.log("---------------- SHIFT! -------------------");
-// console.log(list.shift());
-// console.log("------------------------------------------");
-// console.log({ list });
-
-console.log("------------------------------------------");
-console.log({ list });
-console.log("---------------- PUSH! ------------------");
-console.log(list.push(80));
-console.log("------------------------------------------");
-console.log({ list });
-console.log("---------------- UNSHIFT! -------------------");
-console.log(list.unshift(1));
-console.log("------------------------------------------");
-console.log({ list });
-console.log("---------------- UNSHIFT! -------------------");
-console.log(list.unshift(2));
-
-console.log("------------------------------------------");
-console.log({ list });
-console.log("---------------- POP! -------------------");
-console.log(list.pop());
-console.log("------------------------------------------");
-console.log({ list });
-console.log("---------------- SHIFT! -------------------");
-console.log(list.shift());
-console.log("------------------------------------------");
-console.log({ list });
-
-console.log("---------------- SHIFT! -------------------");
-console.log(list.shift());
-console.log("------------------------------------------");
-console.log({ list });
-console.log("---------------- POP! -------------------");
-console.log(list.pop());
-console.log("------------------------------------------");
-console.log({ list });
+list.push(0);
+list.push(1);
+list.push(2);
+list.push(3);
+list.print();
+list.reverse();
+list.print();
